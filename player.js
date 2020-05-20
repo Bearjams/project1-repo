6a1633dad620ea6dac6693dbb4f729a6976ac02c
bearjam <sam.croce@gmail.com>
2020-05-20 18:49:56 -0400

    song search page working

--- player.js
+++ player.js
@@ -4,8 +4,11 @@
   
     //response.data[0].preview
 
-    
-        var artist = "muse";
+    //var bands = ["muse", "gorillaz", "incubus"]
+
+    //function displayArtistData(){
+
+        var artist = "gorillaz";
 
         var queryURL = "https://api.deezer.com/search?q=" + artist + "&api_key=9f6d61036b12b504cfec594cf6f8dfa6";
   
@@ -14,24 +17,71 @@
           method: "GET"
         })
           .then(function(response) {
+            $("#info-div").text(JSON.stringify(response));
+            // $("#picture-div").text(JSON.stringify(response));
+            // $("##link-div").text(JSON.stringify(response));
+            // $("#info-div").text(JSON.stringify(response));
             console.log(response);
 
-            var name = response.data[0].artist.name;
-            var pic = response.data[0].artist.picture;
-            var link = response.data[0].artist.link;
-            var preview = response.data[0].preview;
-
-
-
+            var name = response.data[i].artist.name;
+            var pic = response.data[i].artist.picture;
+            var link = response.data[i].artist.title;
+            var preview = response.data[i].preview;
             $("#info-div").text(name)
             $("#picture-div").text(pic)
             $("#link-div").text(link)
             $("#preview-div").text(preview)
-
-            
-
-
-
         });
-            
+    //}    
     
+    // $("#find-artist").on("click", function(event){
+
+    //   event.preventDefault();
+
+    //   var artist = $("#artist-input").val();
+
+    //   var queryURL = "https://api.deezer.com/search?q=" + artist + "&api_key=9f6d61036b12b504cfec594cf6f8dfa6";
+
+    //   $.ajax({
+    //     url: queryURL,
+    //     method: "GET"
+    //   }).then(function(response){
+    //     $("#artist-view").text(JSON.stringify(response))
+    //   })
+    // })
+
+    
+    // function renderButtons(){
+
+    //   $("#buttons-view").empty();
+
+    //   for (var i = 0; i < bands.length; i++){
+
+    //     var a = $("<button>");
+
+    //     a.addClass("band");
+        
+    //     a.attr("data-name", bands[i]);
+
+    //     a.text(bands[i]);
+
+    //     $("#buttons-view").append(a)
+    //   }
+
+
+    // }
+
+    // $("#add-artist").on("click", function(event){
+    //   event.preventDefault();
+
+    //   var artist = $("#artist-input").val().trim()
+
+    //   bands.push(artist);
+    //   console.log(bands);
+
+    //   renderButtons()
+    // })
+
+    // $(document).on("click", ".band", displayArtistData);
+
+    // renderButtons
