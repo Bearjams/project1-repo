8ecd0f835b2da8a69be66cf1981b3a8d4f09089b
bearjam <sam.croce@gmail.com>
2020-05-20 09:52:18 -0400

    slight update

--- player.js
+++ player.js
@@ -1,20 +1,10 @@
 
-    // var settings = {
-    //     "async": true,
-    //     "crossDomain": true,
-    //     "url": "https://deezerdevs-deezer.p.rapidapi.com/search?q=muse",
-    //     "method": "GET",
-    //     "headers": {
-    //         "x-rapidapi-host": "deezerdevs-deezer.p.rapidapi.com",
-    //         "x-rapidapi-key": "6b3619b9a7mshcc2e6f44e1b0aa2p176a04jsn19a4d15d645c"
-    //     }
-    // }
+   
+  
+  
+    //response.data[0].preview
 
-    // $.ajax(settings).done(function (response) {
-    //     console.log(response);
-    // });
-
-    //function displayInstrument() {
+    
         var artist = "muse";
 
         var queryURL = "https://api.deezer.com/search?q=" + artist + "&api_key=9f6d61036b12b504cfec594cf6f8dfa6";
@@ -25,6 +15,23 @@
         })
           .then(function(response) {
             console.log(response);
-            });
+
+            var name = response.data[0].artist.name;
+            var pic = response.data[0].artist.picture;
+            var link = response.data[0].artist.link;
+            var preview = response.data[0].preview;
+
+
+
+            $("#info-div").text(name)
+            $("#picture-div").text(pic)
+            $("#link-div").text(link)
+            $("#preview-div").text(preview)
+
             
-    //}        
+
+
+
+        });
+            
+    
